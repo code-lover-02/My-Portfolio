2e0b8036942723a24e0e99ac1464f249c7e3b4cc
Aditya Joshi <79157751+code-lover-02@users.noreply.github.com>
2023-09-09 15:16:07 +0530

    Also check other webpages
    Check my html notes also

---
+++ Personal Portfolio/script.js
@@ -0,0 +1,3 @@
+console.log("Hello!")
+// document.querySelector('.icon2').style.display = 'none';
+document.getElementById('id1').addEventListener("click", ()=>{document.querySelector('.sidebar').classList.toggle('sidebarGo');})
